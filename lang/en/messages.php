b83028ccd91d9b0687bef92147fe1a5e4cba1da4
shar0nnn <anton.bito@gmail.com>
2025-03-25 19:15:58 +0200

    feat: add english localization

---
+++ lang/en/messages.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'Logged in successfully.' => 'Logged in successfully.',
+    'Logged in successfully as parser.' => 'Logged in successfully as parser.',
+    'Wrong email or password.' => 'Wrong email or password.',
+
+    'List of currencies' => 'List of currencies',
+    'Currency created successfully.' => 'Currency created successfully.',
+    'Currency not found.' => 'Currency not found.',
+    'Currency updated successfully.' => 'Currency updated successfully.',
+    'You can not delete a used currency.' => 'You can not delete a used currency.',
+    'Currency deleted successfully.' => 'Currency deleted successfully.',
+
+    'List of pack sizes' => 'List of pack sizes',
+    'Pack size created successfully.' => 'Pack size created successfully.',
+    'Pack size not found.' => 'Pack size not found.',
+    'Pack size updated successfully.' => 'Pack size updated successfully.',
+    'You can not delete a used pack size.' => 'You can not delete a used pack size.',
+    'Pack size deleted successfully.' => 'Pack size deleted successfully.',
+
+    'List of products' => 'List of products',
+    'Error while creating product.' => 'Error while creating product.',
+    'Product created successfully.' => 'Product created successfully.',
+    'Product not found.' => 'Product not found.',
+    'Error while updating product.' => 'Error while updating product.',
+    'Product updated successfully.' => 'Product updated successfully.',
+    'Error while deleting product.' => 'Error while deleting product.',
+    'Product deleted successfully.' => 'Product deleted successfully.',
+
+    'List of retailers' => 'List of retailers',
+    'Error while creating retailer.' => 'Error while creating retailer.',
+    'Retailer created successfully.' => 'Retailer created successfully.',
+    'Retailer not found.' => 'Retailer not found.',
+    'Error while updating retailer.' => 'Error while updating retailer.',
+    'Retailer updated successfully.' => 'Retailer updated successfully.',
+    'Error while deleting retailer.' => 'Error while deleting retailer.',
+    'Retailer deleted successfully.' => 'Retailer deleted successfully.',
+
+    'Error while storing scraped product.' => 'Error while storing scraped product.',
+    'Scraped product stored successfully.' => 'Scraped product stored successfully.',
+];
