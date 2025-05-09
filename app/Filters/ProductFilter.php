0c3894e3ed670d20ccafa37d745fa390c1641d4d
Anton Bytko <anton.bitko@gmail.com>
2025-05-09 12:34:49 +0300

    feat: add filters to products export and create search filter

--- app/Filters/ProductFilter.php
+++ app/Filters/ProductFilter.php
@@ -16,14 +16,14 @@ final class ProductFilter extends QueryStringFilters
         parent::__construct($request);
     }
 
-    public function pack_size_ids($values): void
+    public function pack_size_ids(string $values): void
     {
         $this->eloquentBuilder->whereHas('packSize', function (Builder $query) use ($values) {
             $query->whereIn('id', $this->splitStringIntoArray->handle($values));
         });
     }
 
-    public function retailer_ids($values): void
+    public function retailer_ids(string $values): void
     {
         // Filters input retailer IDs, keeping only those that belong to the currently authenticated user
         $filteredRetailerIds = array_intersect(
@@ -33,6 +33,17 @@ final class ProductFilter extends QueryStringFilters
 
         $this->eloquentBuilder->whereHas('retailers', function (Builder $query) use ($filteredRetailerIds) {
             $query->whereIn('retailers.id', $filteredRetailerIds);
+        });
+    }
+
+    public function search(string $value): void
+    {
+        $value = "%{$value}%";
+
+        $this->eloquentBuilder->where(function (Builder $query) use ($value) {
+            $query->whereLike('title', $value)
+                ->orWhereLike('description', $value)
+                ->orWhereLike('manufacturer_part_number', $value);
         });
     }
 }
