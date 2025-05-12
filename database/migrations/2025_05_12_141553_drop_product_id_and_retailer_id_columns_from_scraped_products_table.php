e60d5d1f87a840fe906f68909de57538518365fe
Anton Bytko <anton.bitko@gmail.com>
2025-05-12 17:58:26 +0300

    refactor: drop product_id, retailer_id and add product_retailer_id columns to scraped_products table

---
+++ database/migrations/2025_05_12_141553_drop_product_id_and_retailer_id_columns_from_scraped_products_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('scraped_products', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('product_id');
+            $table->dropConstrainedForeignId('retailer_id');
+        });
+    }
+};
