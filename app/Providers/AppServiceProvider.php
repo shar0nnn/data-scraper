3923fdf8d790828b9223f2546e3d93863085282f
Anton Bytko <anton.bitko@gmail.com>
2025-05-13 18:14:31 +0300

    feat: add immutable dates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use App\Policies\AdminPolicy;
 use App\Policies\UserPolicy;
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->configureDates();
+
         // Query builder adds a join only if the table hasn't already been joined
         Builder::macro('joinOnce', function ($table, $first, $operator, $second, $type = 'inner') {
             if (collect($this->joins)->pluck('table')->contains($table)) {
@@ -26,5 +30,10 @@ class AppServiceProvider extends ServiceProvider
     {
         Gate::define('admin-crud', [AdminPolicy::class, 'crud']);
         Gate::define('user-crud', [UserPolicy::class, 'crud']);
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
